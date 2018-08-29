2d80f497cbc6921e9c49290b4b77cdb3d8a439e0
Jojolinul <lucianhuza@yahoo.com>
2018-08-29 17:16:27 +0100

    Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,17 @@
 var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+var divArray = ["cubeSlide", "potatoSlide", "towerSlide", "ssgSlide"];
+for(var a = 0; a < divArray.length; a++)
+{
+  showDivs(slideIndex, divArray[a]);
 }
 
-function showDivs(n) {
+function plusDivs(n, divNames) {
+  showDivs(slideIndex += n, divNames);
+}
+
+function showDivs(n, divNames) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName(divNames);
   if (n > x.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
